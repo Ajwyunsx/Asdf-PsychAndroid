5bea7e48c03d1cc516b30673bf2407300293c6bd
Ajwwkxkdang <107187521+Ajwyunsx@users.noreply.github.com>
2024-03-16 00:45:53 +0800

    Update Hitbox.hx

--- source/ui/Hitbox.hx
+++ source/ui/Hitbox.hx
@@ -4,7 +4,7 @@ import flixel.FlxG;
 import flixel.group.FlxSpriteGroup;
 import openfl.display.BitmapData;
 import openfl.display.Shape;
-import flixel.ui.FlxButton;
+import ui.FlxButton;
 
 /**
  * A zone with 4 hint's (A hitbox).
